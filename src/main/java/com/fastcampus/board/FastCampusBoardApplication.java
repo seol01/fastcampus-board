7fd5dd0b34e5b07f1dbeb8ed93d0d2b2f552edf6
김설아 <kimseol-ah@gimseol-aui-MacBookPro.local>
2023-04-02 14:47:08 +0900

    init

---
+++ src/main/java/com/fastcampus/board/FastCampusBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusBoardApplication.class, args);
+    }
+
+}
